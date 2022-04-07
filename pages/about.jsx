5a140c886d60695a09d22a658569de44d2b64097
Isaac <canochaba@gmail.com>
2022-04-07 13:16:21 +0200

    Primer commit

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+  return (
+    <Layout
+        title= "About | next.js"
+        description = "Agregue una descripcion">
+        <h1>About</h1>
+    </Layout>
+  )
+}
